f902233651272c4317abc91b6e6b67ef73f1f431
hanteng <hanteng@gmail.com>
2015-03-11 17:07:36 +0100

    version number changed

--- pyGeolinguisticSize/__init__.py
+++ pyGeolinguisticSize/__init__.py
@@ -6,7 +6,7 @@ from __future__ import (absolute_import, division, print_function,
 # This is the only place the version is specified and 
 # used in both setup.py and docs/conf.py to set the 
 # version of ggplot.
-__version__ = '0.0.0.1'
+__version__ = '0.0.0.2'
 
 from .sizedb import (mapping, langname, territory, size_gl, size_l, pgviews_wiki)
 
