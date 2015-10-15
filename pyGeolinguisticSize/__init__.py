68d8c6ef667e109eecb276f0104fe1119289d2da
Han-Teng Liao <hanteng@gmail.com>
2015-10-15 13:31:00 +0800

    geolinguistic installing issue
    geolinguistic installing issue

--- pyGeolinguisticSize/__init__.py
+++ pyGeolinguisticSize/__init__.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
-#歧視無邊，回頭是岸。鍵起鍵落，情真情幻。
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 # This is the only place the version is specified and 
 # used in both setup.py and docs/conf.py to set the 
 # version of ggplot.
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 from .sizedb import (wiki_sitemap, wiki_pageviews, mapping, langname, territory, territory_lang, size_gl, size_gl_IPop, size_l, size_l_wiki,)
 
